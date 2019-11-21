c1fc204201de5bbdbb122972b82920890e76050e
Rachel Conatser <rconatser7@gmail.com>
2019-11-20 23:52:12 -0700

    added image src array to add to food list

--- src/components/pages/OurMenu.vue
+++ src/components/pages/OurMenu.vue
@@ -6,7 +6,7 @@
 
 <template>
     <v-content class="pt-0">
-        <v-parallax class="title-container" height="500" src="../../assets/food-items/tonkotsu-ramen.jpg">
+        <v-parallax class="title-container" height="500" src="<img src='/tonkotsu-ramen.jpg' alt='' />">
           <h1 class="title-text"><span class="font-weight-bold">Our </span>Menu</h1>
         </v-parallax>
         <div class="text-container">
@@ -18,7 +18,8 @@
         <div class="text-container">
           <h2 class="text-center">Our Ingredients and their Nutrition Information</h2>
           <ul>
-            <li v-for="ingredient in ingredients" :key="ingredient"><span class="headline condensed light">{{ ingredient.key }}</span> <span class="items">
+            <li v-for="ingredient in ingredients" :key="ingredient">
+              <span class="image">{{ imageSrc }}</span><span class="headline condensed light">{{ ingredient.key }}</span> <span class="items">
                 <span>{{ ingredient.calories }} Calories</span>
                 <span>{{ ingredient.carbs }}g Carbs</span>
                 <span>{{ ingredient.fat }}g Fat</span>
@@ -44,7 +45,8 @@ import axios from 'axios';
 export default {  
   data() {
     return {
-      ingredients: []
+      ingredients: [],
+      imageSrc: ["<img src='/chicken-broth.jpg' alt='chicken broth' />","<img src='/beef-broth.jpg' alt='beef broth' />", "<img src='/miso-broth.jpeg' alt='miso broth' />","<img src='/shoyu-broth.jpg' alt='shoyu broth' />", "<img src='/shio-broth.jpg' alt='shio broth' />", "<img src='/tonkotsu-broth.jpg' alt='tonkotsu broth' />", "<img src='/chicken.jpg' alt='chicken' />", "<img src='/beef.jpg' alt='beef' />", "<img src='/chasu.jpg' alt='chasu' />", "<img src='/shrimp.jpg' alt='shrimp' />", "<img src='/green-onion.jpg' alt='green onions' />", "<img src='/onion.jpg' alt='onion' />", "<img src='/broccoli.jpg' alt='broccoli' />", "<img src='/carrot.jpg' alt='carrots' />", "<img src='/mushrooms.jpg' alt='mushrooms' />", "<img src='/corn.jpg' alt='corn' />", "<img src='/cilantro.jpg' alt='cilantro' />", "<img src='/lime.jpg' alt='lime' />", "<img src='/egg.jpg' alt='eggs' />", "<img src='/red-pepper.jpg' alt='red bell pepper' />", "<img src='/seseme-seeds.jpg' alt='seseme seeds' />", "<img src='/nori.jpg' alt='nori' />", "<img src='/naruto.jpg' alt='naruto' />"]
     }
   },
   created() {
@@ -87,4 +89,9 @@ export default {
     display: block;
   }
   
+  ul li span.image {
+    display: block;
+    height: 100px;
+    width: 100px;
+  }
 </style>
